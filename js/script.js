d2c639d0da26b38a87bcdb4d0185565e3b0c794f
Dmitriy <dmitriy.ponomarenko2001@gmail.com>
2020-10-15 00:48:32 +0300

    cycle_lesson015_15.10.2020

--- js/script.js
+++ js/script.js
@@ -7,12 +7,62 @@ const personalMovieDB = {
     privat:false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?', '');
+let a,b;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+ for (let i =0; i<=1; i++){
+    a = prompt('Один из последних просмотренных фильмов?','');
+   
+    if (a == ""|| a == null)
+    {
+        while(a == ""|| a == null){
+        a = prompt('Ошибка. Введите название фильма.', '');
+            if(a.length>50){
+                while (a.length>50){
+                    a = prompt('Ошибка. Введите название как-нибудь покороче.', '');
+                }
+            }
+         }
+    } else if(a.length>50){
+        while (a.length>50){
+        a = prompt('Ошибка. Введите название как-нибудь покороче.', '');
+    }
+        if (a == ""|| a == null){
+            while(a == ""|| a == null){
+            a = prompt('Ошибка. Введите название фильма.', '');
+            }
+        }   
+    }
+    b = prompt('На сколько оцените его?', '');
+    if (b == ""|| b == null)
+    {
+        while(b == ""|| b == null){
+        b = prompt('Ошибка. Оцените фильм.', '');
+            if(b>100){
+                while (b>100){
+                    b = prompt('Ошибка. Дайте более объективную оценку.', '');
+                }
+            }
+         }
+    } else if(b>100){
+        while (b>100){
+            b = prompt('Ошибка. Дайте более объективную оценку.', '');
+    }
+        if (b == ""|| b == null){
+            while(b == ""|| b == null){
+                b = prompt('Ошибка. Оцените фильм.', '');
+            }
+        }   
+    }
+    personalMovieDB.movies[a] = b;
+ }  
 
 console.log(personalMovieDB);
+ if (personalMovieDB.count < 10 && personalMovieDB.count != null && personalMovieDB.count != 0){
+     alert('Просмотрено довольно мало фильмов');
+ } else if (personalMovieDB.count < 30 && personalMovieDB.count != null && personalMovieDB.count != 0){
+     alert('Вы классический зритель');
+ } else if (personalMovieDB.count > 30 && personalMovieDB.count != null && personalMovieDB.count != 0){
+     alert('Вы киноман');
+ } else{
+     alert('error');
+ }
